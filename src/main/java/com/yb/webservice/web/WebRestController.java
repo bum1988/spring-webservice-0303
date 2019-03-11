f7975307cb247b6749ff95ae344c736a461dd110
bum1988 <bum1988@naver.com>
2019-03-11 21:39:14 +0900

    프로젝트 생성

---
+++ src/main/java/com/yb/webservice/web/WebRestController.java
@@ -0,0 +1,19 @@
+package com.yb.webservice.web;
+
+import com.yb.webservice.Response;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello  KaKaoPay Team!!";
+    }
+
+    @RequestMapping("/health")
+    public Response healthCheck() {
+        return new Response("success", "It works well!!");
+    }
+}
